0496246e1fddc3785bb839e867c5f0c75c9662c5
MCHSL <michal.sul+githubalt@wp.pl>
2022-11-28 15:46:24 +0100

    yeet redux

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,14 +1,8 @@
-import { useState } from "react";
 import "./App.css";
 import React from "react";
-import { useSelector } from "react-redux";
 import LoginForm from "./app/components/LoginForm";
 
 function App() {
-  const directory = useSelector((state: any) => state.directoryList);
-
-  console.log(directory);
-
   return (
     <div className="flex justify-center">
       <LoginForm />
